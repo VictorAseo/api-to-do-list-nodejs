cd81989c99f6ba7790c263b23ea15ece8476dc4c
Joseph Haase <joseph.lima@codenapp.com>
2023-04-22 16:56:11 -0300

    Habilitação do cors

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,16 @@ import { Application } from "express";
 import { router } from "./routes/router";
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
+
 const app: Application = express();
 const PORT: string | number = process.env.PORT || 8686;
 
 app.listen(PORT, (): void => console.log("Server running on 8686"));
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/", router);
