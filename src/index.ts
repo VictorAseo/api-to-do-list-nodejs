22ae0f03958e89f654a21bf41450fd84ca2ac2f2
Joseph Haase <joseph.haase.dev@gmail.com>
2023-04-23 17:35:48 -0300

    Ajuste na configuração da vercel

--- src/index.ts
+++ src/index.ts
@@ -17,5 +17,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
-app.use(express.static("vercel/output"));
+app.use(express.static("public"));
 app.use("/", router);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
